a95b4dc29883c938a9a2141116321bd31a2ffc9a
Sam Adesoga <samueladesoga@gmail.com>
2023-08-03 23:49:14 +0100

    add code for launching the compare basket

--- cypress/e2e/load-page.cy.js
+++ cypress/e2e/load-page.cy.js
@@ -1,9 +1,20 @@
+//import CompareBasket from '../pages/CompareBasket';
+    Cypress.on('uncaught:exception', (err) => {
+      console.log('Cypress detected uncaught exception: ', err);
+      return false;
+    });
+
 describe('crm software compare page', () => {
   beforeEach(() => {
     cy.visit('/crm')
   })
 
   it('displays compare basket button', () => {
+    //const compareBasket = new CompareBasket();
     cy.get('div[data-testid=appbar_compare-basket_button] span').should('have.text', 0)
+    cy.get('div[data-testid=appbar_compare-basket_button] span').click()
+    cy.get('div[data-testid=compare-basket]').should('be.visible')
+    //expect(compareBasket.isDisplayed()).to.be.true
+    //expect(compareBasket.basketEmpty()).to.be.true
   })
 })
